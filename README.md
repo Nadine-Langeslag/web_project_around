35569a1d21a97a1e652f3ef880ab7bf30744bd71
Nadine <nadine.langeslag@gmail.com>
2025-07-30 18:54:57 -0500

    first atempt for my project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tripleten web_project_around
+
+First proyect working with JavaScript, working on making a popop-form and changing the profile information
---
+++ index.js
@@ -0,0 +1,35 @@
+let profileName = document.querySelector(".profile__name");
+let profileAbout = document.querySelector(".profile__ocupation");
+let profileForm = document.querySelector(".form");
+let nameInput = document.querySelector(".form__input_name");
+let aboutInput = document.querySelector(".form__input_about");
+let saveButton = document.querySelector(".form__save-button")
+let formInput = document.querySelector(".form__input")
+
+let profileEditButton = document.querySelector(".profile__edit-button")
+let formCloseButton = document.querySelector(".form__close-button")
+
+
+
+function editProfile (event){
+    event.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+}
+
+profileForm.addEventListener("submit", editProfile)
+
+profileEditButton.addEventListener("click",function(){
+    profileForm.classList.add("form_open")
+})
+
+formCloseButton.addEventListener("click",function(){
+    profileForm.classList.remove("form_open")
+})
+
+saveButton.addEventListener("input", function(){
+    if (formInput.value() !== " "){
+        saveButton.style.background = black;
+        saveButton.style.color = white;
+    }
+})
